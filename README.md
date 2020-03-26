fc3ad903f9deb4ca0682245c39f6a0e61b722fe5
Thibaut Vercueil <thibaut.vercueil@st.com>
2020-03-26 10:48:04 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# microtrace
-Tracing tool for microcontrollers
+# Microtrace
+
+```
+  /\/\ (_) ___ _ __ ___/__   \_ __ __ _  ___ ___ 
+ /    \| |/ __| '__/ _ \ / /\/ '__/ _` |/ __/ _ \
+/ /\/\ \ | (__| | | (_) / /  | | | (_| | (_|  __/
+\/    \/_|\___|_|  \___/\/   |_|  \__,_|\___\___|
+
+```
+
+Tracing tool for microcontrollers. Inspired from [minitrace](https://github.com/hrydgard/minitrace). 
+
+Microtrace is a header-only library targeting microcontrollers for producing JSON traces suitable for Chrome's built-in trace viewer (chrome://tracing).
+
+> Note: Is in very early developpement and currently has only been tested on STM32 microcontrolers
+
+## Screenshots
+
+Comming soon
+
+## Prerequisites
+
+
+
+
+## How to use 
+
+Copy paste microtrace.h into your project, include the file and use it like this:
+
+```c
+#include "microtrace.h"
+
+
+void main(void){
+
+    MICROTRACE_INIT();
+
+		/* ... */
+
+    MICROTRACE_BEGIN("Perif", "Camera Init");
+		BSP_Camera_Init();
+    MICROTRACE_END("Perif", "Camera Init");
+
+
+    MICROTRACE_BEGIN("Main", "Main loop");
+		for(int i = 0; i < 10; i++){
+
+        MICROTRACE_BEGIN("Image proc", "Equalize histogram")
+				/* Processing here */
+        MICROTRACE_END("Image proc", "Equalize histogram")
+
+
+		}
+    MICROTRACE_BEGIN("Main", "Main loop");
+
+    MICROTRACE_INSTANT("Main", "Instantaneous event");
+
+
+    MICROTRACE_DUMP_JSON();
+}
+
+
+```
+
+`MICROTRACE_DUMP_JSON()` will dump a json file through `printf()`, if you are using a UART, you can do 
+
+```sh
+cat /dev/ttyACMX > trace.json
+```
+
+We will then be able to open the trace with Chrome tracing tool by openning chrome://tracing and clicking `Load button`.
+
+Please refer to Chrome documentation on how to use this tool.
+
+> Note: If the maximum number of events is reached (see Configuration). `MICROTRACE_DUMP_JSON()` will be automatically called and the program will go in infinite loop.
+
+## Configuration 
+
+Please refer to minitrace.h 
+
+## Licence 
+
+This work is under MIT Licence. See Licence file for more info
---
+++ microtrace.h
@@ -0,0 +1,160 @@
+/*
+ *
+ * This file is part of the microtrace project.
+ * Copyright (c) 2020 Thibaut Vercueil
+ * This work is licensed under the MIT license, see the file LICENSE for
+ * details.
+ *
+ * =====================================================================================
+ *           dGGGGMMb     ,"""""""""""""".
+ *          @p~qp~~qMb    | MicroTrace ! |
+ *          M|@||@) M|   _;..............'
+ *          @,----.JM| -'
+ *         JS^\__/  qKL
+ *        dZP        qKRb
+ *       dZP          qKKb
+ *      fZP            SMMb
+ *      HZM            MMMM
+ *      FqM            MMMM
+ *    __| ".        |\dS"qML
+ *    |    `.       | `' \Zq
+ *   _)      \.___.,|     .'
+ *   \____   )MMMMMM|   .'
+ *     `-'       `--'
+ *
+ *	Ultra-light library for tracing on microcontrollers
+ * =====================================================================================
+ */
+#ifndef _MICROTRACE_H_
+#define _MICROTRACE_H_
+#include <stdint.h>
+#include <stdio.h>
+
+/* =============================== */
+/* ====== Configuration ========== */
+/* =============================== */
+
+/* Uncomment the following to disable Microtrace */
+/* #define MICROTRACE_DISABLE */
+
+/* Maximum number of events to be recorded. Microtrace will stop after max
+ * events is reached and start to jump the JSON report. A big number of event
+ * can take a lot of space in RAM */
+#define MICROTRACE_MAX_EVENTS (5000)
+
+/* Maximum number of characters for the `Category` or `Name` string
+ * if an string lenght exceeded this number the compilation will fail
+ * Note that reducing this value improves memory footprint */
+#define MICROTRACE_MAX_STRING_LEN (32)
+
+/* Uncomment the followint if you want to place
+ * Microtrace buffers in specific memory sections
+ * (see MICROTRACE_BUFFER_SECTION) */
+/* #define MICROTRACE_CUSTOM_MEMORY */
+
+/* Memory section to place microtrace buffer.
+ * (it can be usefull to use external memory if avaible. e.g ".ext_sdram"
+ * Warning MICROTRACE_CUSTOM_MEMORY should be defined */
+#define MICROTRACE_BUFFER_SECTION ""
+
+/* Function used to get number of microseconds elapsed since begining of
+ * program. e.g HAL_GetTick */
+#define MICROTRACE_COUNT_FUNCTION HAL_GetTick
+
+/* If MICROTRACE_COUNT_FUNCTION doesn't return a number of microseconds, a
+ * multiplier can be applied to convert to microseconds. e.g HAL_GetTick()
+ * returns miliseconds, hence x1000 */
+#define MICROTRACE_MULTIPLIER (1000)
+
+/* =============================== */
+
+typedef struct {
+  char category[MICROTRACE_MAX_STRING_LEN];
+  char name[MICROTRACE_MAX_STRING_LEN];
+  uint32_t timestamp;
+  char phase;
+} microtrace_event_t;
+
+#define SIZE_OF sizeof(MICROTRACE_BUFFER_SECTION)
+
+#ifdef MICROTRACE_CUSTOM_MEMORY
+__attribute__((section(MICROTRACE_BUFFER_SECTION)))
+#endif
+microtrace_event_t microtrace_event_buffer[MICROTRACE_MAX_EVENTS];
+uint32_t microtrace_event_counter;
+
+#ifdef MICROTRACE_CUSTOM_MEMORY
+__attribute__((section(MICROTRACE_BUFFER_SECTION)))
+#endif
+char microtrace_print_buffer[512];
+
+#define MICROTRACE_STATIC_ASSERT(COND, MSG)                                    \
+  typedef char MICROTRACE_STATIC_ASSERTION_FAILED_##MSG[(COND) ? 1 : -1]
+
+#define MICROTRACE_INIT()                                                      \
+  do {                                                                         \
+    microtrace_event_counter = 0;                                              \
+  } while (0);
+
+#ifndef MICROTRACE_DISABLE
+#define _MICROTRACE_ADD_EVENT(_phase, _category, _name)                        \
+  do {                                                                         \
+    MICROTRACE_STATIC_ASSERT(sizeof(_category) < MICROTRACE_MAX_STRING_LEN,    \
+                             CATEGORY_STRING_LENGHT_TOO_BIG);                  \
+    MICROTRACE_STATIC_ASSERT(sizeof(_name) < MICROTRACE_MAX_STRING_LEN,        \
+                             NAME_STRING_LENGHT_LENGHT_TOO_BIG);               \
+    if (microtrace_event_counter >= MICROTRACE_MAX_EVENTS) {                   \
+      MICROTRACE_DUMP_JSON();                                                  \
+      while (1) {                                                              \
+      };                                                                       \
+    }                                                                          \
+    uint32_t timestamp = MICROTRACE_COUNT_FUNCTION();                          \
+    microtrace_event_t *event =                                                \
+        &microtrace_event_buffer[microtrace_event_counter];                    \
+    microtrace_event_counter++;                                                \
+    strcpy(event->category, _category);                                        \
+    strcpy(event->name, _name);                                                \
+    event->timestamp = timestamp;                                              \
+    event->phase = _phase;                                                     \
+  } while (0);
+#else
+#define _MICROTRACE_ADD_EVENT(_phase, _category, _name)                        \
+  do {                                                                         \
+  } while (0)
+#endif
+
+#ifndef MICROTRACE_DISABLE
+#define MICROTRACE_DUMP_JSON()                                                 \
+  do {                                                                         \
+    printf("{\"traceEvents\":[\r\n");                                          \
+    for (uint32_t i = 0; i < microtrace_event_counter - 1; i++) {              \
+      sprintf(microtrace_print_buffer,                                         \
+              "{\"cat\":\"%s\",\"pid\":1,\"tid\":1,\"ts\":%ld,\"ph\":\"%c\","  \
+              "\"name\":\"%s\",\"args\":{}},\r\n",                             \
+              microtrace_event_buffer[i].category,                             \
+              microtrace_event_buffer[i].timestamp *MICROTRACE_MULTIPLIER,     \
+              microtrace_event_buffer[i].phase,                                \
+              microtrace_event_buffer[i].name);                                \
+      printf(microtrace_print_buffer);                                         \
+    }                                                                          \
+    sprintf(microtrace_print_buffer,                                           \
+            "{\"cat\":\"%s\",\"pid\":1,\"tid\":1,\"ts\":%ld,\"ph\":\"%c\","    \
+            "\"name\":\"%s\",\"args\":{}}]}\r\n",                              \
+            microtrace_event_buffer[microtrace_event_counter - 1].category,    \
+            microtrace_event_buffer[microtrace_event_counter - 1]              \
+                .timestamp *MICROTRACE_MULTIPLIER,                             \
+            microtrace_event_buffer[microtrace_event_counter - 1].phase,       \
+            microtrace_event_buffer[microtrace_event_counter - 1].name);       \
+    printf(microtrace_print_buffer);                                           \
+  } while (0)
+#else
+#define MICROTRACE_DUMP_JSON()                                                 \
+  do {                                                                         \
+  } while (0)
+#endif
+
+#define MICROTRACE_BEGIN(cat, name) _MICROTRACE_ADD_EVENT('B', cat, name)
+#define MICROTRACE_END(cat, name) _MICROTRACE_ADD_EVENT('E', cat, name)
+#define MICROTRACE_INSTANT(cat, name) _MICROTRACE_ADD_EVENT('I', cat, name)
+
+#endif // _MICROTRACE_H_
